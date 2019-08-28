532d7385e30fb5966121a82d18271837d88e90c9
wsx1887 <wsx1887@qq.com>
2019-08-28 13:22:27 +0800

    Signed-off-by: wsx1887 <wsx1887@qq.com>

---
+++ myWork/main.js
@@ -0,0 +1,42 @@
+const electron = require('electron');
+
+const { app, BrowserWindow } = require('electron');
+
+// 保持对window对象的全局引用，如果不这么做的话，当JavaScript对象被垃圾回收的时候，window对象将会自动的关闭。
+let win;
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+    // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
+    // 否则绝大部分应用及其菜单栏会保持激活。
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+app.on('activate', () => {
+    // 在macOS上，当单击dock图标并且没有其他窗口打开时，
+    // 通常在应用程序中重新创建一个窗口。
+    if (win === null) {
+        createWindow()
+    }
+});
+
+function createWindow() {
+    // 创建浏览器窗口
+    let win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    // 加载index.html文件
+    win.loadFile('index.html');
+    // 当 window 被关闭，这个事件会被触发。
+    win.on('closed', () => {
+        // 取消引用 window 对象，如果你的应用支持多窗口的话，
+        // 通常会把多个 window 对象存放在一个数组里面，
+        // 与此同时，你应该删除相应的元素。
+        win = null
+    });
+}
