8aa064f84a6f6b2b327b58327a9ebb1dccd021db
wsx1887 <wsx1887@qq.com>
2019-09-03 17:58:34 +0800

    1

--- myWork/main.js
+++ myWork/main.js
@@ -1,9 +1,9 @@
-const createMenu=require('./src/main-process/app-menu.js');
-const { BrowserView,app, BrowserWindow, Menu } = require('electron');
+const createMenu = require('./src/main-process/app-menu.js');
+const { app, BrowserWindow, Menu } = require('electron');
 
 // 保持对window对象的全局引用，如果不这么做的话，当JavaScript对象被垃圾回收的时候，window对象将会自动的关闭。
 let win;
-let menu=createMenu();
+let menu = createMenu();
 Menu.setApplicationMenu(menu);
 
 app.on('ready', createWindow);
@@ -11,14 +11,14 @@ app.on('window-all-closed', () => {
     // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
     // 否则绝大部分应用及其菜单栏会保持激活。
     if (process.platform !== 'darwin') {
-        app.quit()
+        app.quit();
     }
 });
 app.on('activate', () => {
     // 在macOS上，当单击dock图标并且没有其他窗口打开时，
     // 通常在应用程序中重新创建一个窗口。
     if (win === null) {
-        createWindow()
+        createWindow();
     }
 });
 
@@ -32,16 +32,12 @@ function createWindow() {
         }
     });
     // 加载index.html文件
-    win.loadFile('index.html');
-    let view = new BrowserView()
-win.setBrowserView(view)
-view.setBounds({ x: 0, y: 0, width: 300, height: 300 })
-view.webContents.loadURL('https://electronjs.org')
+    win.loadFile('./src/renderer-process/html/sendemail.html');
     // 当 window 被关闭，这个事件会被触发。
     win.on('closed', () => {
         // 取消引用 window 对象，如果你的应用支持多窗口的话，
         // 通常会把多个 window 对象存放在一个数组里面，
         // 与此同时，你应该删除相应的元素。
-        win = null
+        win = null;
     });
 }
--- myWork/src/renderer-process/js/sendemail.js
+++ myWork/src/renderer-process/js/sendemail.js
@@ -1,2 +1,113 @@
-let nodemailer=require('nodemailer');
+let nodemailer = require('nodemailer');
+let axios = require('axios');
+let fs = require('fs');
 
+(function () {
+    let transporter = nodemailer.createTransport({
+        pool: true,
+        host: 'smtp.qq.com',
+        port: 465,
+        srcure: true,
+        auth: {
+            user: 'wsx1887@qq.com',
+            pass: 'lqosoxejsxjjbeaf'
+        }
+    });
+    //"发送邮件"按钮的单击事件处理
+    document.getElementById('sendemail').addEventListener('click', async function sendemailArray(event) {
+        let imgsArray = document.getElementById('imgs-list').value.split("\n").filter(item => item.trim().length > 0);
+        let attachmentArray = document.getElementById('attachments-list').value.split("\n").filter(item => item.trim().length > 0);
+        let basepath = document.getElementById('path').value.replace(/\\/g, "/");
+        if (basepath[basepath.length - 1] != "/") {
+            basepath += "/";
+        }
+        let sendMessageArray = [];
+        for (let i = 0; i < attachmentArray.length; i++) {
+            let message = JSON.parse(JSON.stringify(messageModel));
+            message.subject = attachmentArray[i];
+            message.attachments[0] = {
+                path: basepath + attachmentArray[i]
+            };
+            if (imgsArray.length > 0) {
+                //图片比附件少时轮流使用图片
+                let n = i;
+                while (n > imgsArray.length - 1) {
+                    n -= imgsArray.length;
+                }
+                message.html = `<img src="cid:${i}"/>`;
+                message.attachments[1] = {
+                    path: basepath + imgsArray[n],
+                    cid: i + ''//隐式转换成string
+                };
+            }
+            sendMessageArray.push(message);
+        }
+        //添加消息框内容并显示
+        let divSendingMessage = document.querySelector('.sending-message');
+        divSendingMessage.innerHTML = '';
+        let elementList = [];//用于更改‘发送状态’
+        for (let i = 0; i < sendMessageArray.length; i++) {
+            let divRow = document.createElement('div');
+            divRow.className = 'row';
+            divRow.innerHTML = `<div class="attach-name">${sendMessageArray[i].subject}</div>`;
+            let divSending = document.createElement('div');
+            divSending.className = 'sending';
+            divSending.innerHTML = '已准备';
+            divRow.appendChild(divSending);
+            divSendingMessage.appendChild(divRow);
+            elementList[i] = divSending;
+        }
+        document.querySelector('.message-container').style.display = 'block';
+        for (let i = 0; i < sendMessageArray.length; i++) {
+            elementList[i].innerHTML = '正在发送';
+
+            let res = await sendMailAsync(sendMessageArray[i]);
+            if (res === 'err') {
+                elementList[i].innerHTML = '失败';
+            } else {
+                elementList[i].innerHTML = '发送成功';
+            }
+        }
+    });
+    //消息窗的关闭按钮
+    document.getElementById("button-close").addEventListener('click', event => {
+        document.querySelector('.message-container').style.display = 'none';
+    })
+    //自动扫描目录
+    document.getElementById('scan').addEventListener('click', event => {
+        let basepath = document.getElementById('path').value.replace(/\\/g, "/");
+        let filePathArray = fs.readdirSync(basepath);
+        if (filePathArray.length > 0) {
+            let imgsString = '';
+            let attachmentString = '';
+            filePathArray.forEach(item => {
+                let extensionName = item.substring(item.lastIndexOf('.'));
+                if (extensionName == '.rar' || extensionName == '.zip' || extensionName == '.7z') {
+                    attachmentString += `${item}\n`;
+                } else {
+                    imgsString += `${item}\n`;
+                }
+            });
+            document.getElementById('imgs-list').value = imgsString;
+            document.getElementById('attachments-list').value = attachmentString;
+        }
+
+    });
+    //邮件消息格式
+    let messageModel = {
+        from: 'wsx1887@qq.com',
+        to: 'wsx1887@qq.com',
+        subject: '',
+        html: '',
+        attachments: [{}]
+    };
+    //封装发送邮件为promise
+    async function sendMailAsync(message) {
+        return new Promise((resolve, reject) => {
+            transporter.sendMail(message, (err, info) => {
+                if (err) { reject('err') }
+                else { resolve(info) }
+            })
+        })
+    }
+})();
